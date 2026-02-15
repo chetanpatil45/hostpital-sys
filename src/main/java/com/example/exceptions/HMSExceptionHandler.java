123cb21214e5f11b42f49e9a3a4894f4a4b7b8c8
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-02-16 00:00:16 +0530

    added ControllerAdvice class to handle exceptions

--- src/main/java/com/example/exceptions/HMSExceptionHandler.java
+++ src/main/java/com/example/exceptions/HMSExceptionHandler.java
@@ -1,8 +1,14 @@
 package com.example.exceptions;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestController
 public class HMSExceptionHandler {
 
+    @ExceptionHandler({RuntimeException.class, NullPointerException.class})
+    public String handleException(Exception e){
+        return "ERROR OCCURRED";
+    }
 }
